57e077946940e86879ed58718e6c99fef6d1fa7e
NickVatne <nicovatne@gmail.com>
2019-02-19 20:09:01 +0100

    Task Managers

--- Database/src/main/java/com/kristiania/pgr200/database/dao/TaskManagerDao.java
+++ Database/src/main/java/com/kristiania/pgr200/database/dao/TaskManagerDao.java
@@ -1,4 +1,48 @@
 package com.kristiania.pgr200.database.dao;
 
-public class TaskManagerDao {
+import com.kristiania.pgr200.database.entity.TaskManager;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+public class TaskManagerDao extends AbstractDao {
+    private String sqlGET = "SELECT * FROM TASKMANAGER";
+    private String sqlSave = "INSERT INTO TASKMANAGER (FIRST_USER, SECOND_USER, THIRD_USER) VALUES (?, ?, ?)";
+
+    public TaskManagerDao(DataSource dataSource){
+        super(dataSource);
+    }
+    public List<TaskManager> getAll () throws SQLException{
+        return list(sqlGET, this::mapToManager);
+    }
+
+    public void save(TaskManager taskManager) throws SQLException{
+        try (Connection connection = dataSource.getConnection()){
+            try (PreparedStatement statement = connection.prepareStatement(sqlSave, PreparedStatement.RETURN_GENERATED_KEYS)){
+                statement.setString(1, taskManager.getFirstUser());
+                statement.setString(1, taskManager.getSecondUser());
+                statement.setString(1, taskManager.getThirdUser());
+                statement.executeUpdate();
+                try (ResultSet resultSet = statement.getGeneratedKeys()){
+                    resultSet.next();
+                    taskManager.setId(resultSet.getInt(1));
+                }
+            }
+        }
+    }
+
+    private TaskManager mapToManager (ResultSet rs) throws SQLException{
+        TaskManager taskManager = new TaskManager();
+        taskManager.setId(rs.getInt("ID"));
+        taskManager.setFirstUser(rs.getString("FIRST_USER"));
+        taskManager.setFirstUser(rs.getString("SECOND_USER"));
+        taskManager.setFirstUser(rs.getString("THIRD_USER"));
+
+        return taskManager;
+
+    }
 }
